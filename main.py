fcd620e3055c41b3ea7119f8f4dd7bffe90d09ef
User <user@gmail.com>
2021-12-11 17:54:40 +0300

    #1

---
+++ main.py
@@ -0,0 +1,12 @@
+def my_superfunction():
+    print('WHOW!!! AWESOME!')
+
+
+def main():
+    print('my first git!')
+    print('and i change it')
+    print('again')
+    my_superfunction()
+
+
+main()
